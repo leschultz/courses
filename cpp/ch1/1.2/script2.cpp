3109cd17e1331bad66de4fefcd79fb0825ed3527
nerve <nerve@pop-os.localdomain>
2022-01-02 19:49:55 -0600

    Now done with simple comments

---
+++ cpp/ch1/1.2/script2.cpp
@@ -0,0 +1,13 @@
+/* This is now a block comment
+ * that is separated nicely for 
+ * easy reading.
+*/
+
+#include <iostream>
+
+int main()
+{
+    // This is my comment
+    std::cout << "Hello again";
+    return 0;
+}
