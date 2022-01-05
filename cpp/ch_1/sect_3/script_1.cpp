5163fbdcb176ffbed44aba7a963122f2477e78ab
nerve <nerve@pop-os.localdomain>
2022-01-04 19:59:49 -0600

    Now included another section

---
+++ cpp/ch_1/sect_3/script_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    // Here is one comment
+    /* Here is a block comment
+     */
+
+    int a;
+    double b;
+
+    std::cout << "The value of a and b are defined within";
+}
