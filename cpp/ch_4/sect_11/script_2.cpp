7dc306987ceb391ee5205fd6adb3565e13a85f29
leschultz <laneenriqueschultz@gmail.com>
2022-02-27 09:08:55 -0600

    Learned about characters

---
+++ cpp/ch_4/sect_11/script_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Input character: ";
+    char ch{};
+    std::cin >> ch;
+    std::cout << "You entered: " << ch << '\n';
+
+    return 0;
+}
---
+++ cpp/ch_4/sect_11/script_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    char ch{};
+    std::cin >> ch;
+    std::cout << "You entered: " << ch << '\n';
+
+    std::cin >> ch;
+    std::cout << "You entered: " << ch << '\n';
+
+    return 0;
+}
