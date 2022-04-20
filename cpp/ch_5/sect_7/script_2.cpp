0b2361c2ee66d171698648342dd6164cfa1466a4
leschultz <laneenriqueschultz@gmail.com>
2022-04-19 19:08:18 -0500

    Learned about logical operators

---
+++ cpp/ch_5/sect_7/script_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Enter a number: ";
+    int value {};
+    std::cin >> value;
+
+    if (value == 0 || value == 1)
+        std::cout << "You picked 0 or 1\n";
+    else
+        std::cout << "You did not pick 0 or 1";
+    return 0;
+}
---
+++ cpp/ch_5/sect_7/script_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Enter a number: ";
+    int value {};
+    std::cin >> value;
+
+    if (value > 10 && value < 20)
+        std::cout << "Your value is between 10 and 20\n";
+    else
+        std::cout << "Your value is not between 10 and 20\n";
+    return 0;
+}
